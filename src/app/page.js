11458d3b516be2eb49a097320897478df65cf571
Danny Xu <dax002@ucsd.edu>
2025-04-24 13:52:13 -0700

    dd

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl font-bold">Hello World</h1>
+        <h1 className="text-3xl font-semibold">Hello World</h1>
+        <h2 className="text-2xl font-normal">I am a Software Engineer</h2>
       </main>
       
     </div>
