d7b8a9f065fce9af92690937acd1a519aca99d2d
Danny Xu <dax002@ucsd.edu>
2025-04-29 13:42:21 -0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
         <h2 className="text-2xl font-normal">My name is Danny Xu</h2>
         <h3 className="text-xl font-light">This is my github page</h3>
         <h4 className="text-lg font-thin">I am a software engineer</h4>
+        <h5 className="text-base font-extralight">I am a software engineer</h5>
         <button
           className="px-4 py-2 text-white bg-red-500 rounded"
           onClick={(e) => {
