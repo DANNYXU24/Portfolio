b68940fac09f93310507ec46e7f21bc935de688a
Danny Xu <dax002@ucsd.edu>
2025-05-01 12:56:12 -0700

    config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-const isGitHubPages = process.env.NODE_ENV === 'production';
-const repo = 'Portfolio'; // replace with your actual repo name
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isGitHubPages ? `/${repo}` : '',
-  assetPrefix: isGitHubPages ? `/${repo}/` : '',  // Include this line for assets
-  trailingSlash: true,
+  output: 'standalone',  // Use standalone instead of export when deploying to Vercel (if you're using server-side features)
+  trailingSlash: true,    // Important for static routing to Vercel
 };
 
 export default nextConfig;
-// If you want to use the `next build` command, you can add the following line
-
