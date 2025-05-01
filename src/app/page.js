0fad2beac0091a815b568c7c269dd5032a202414
Danny Xu <dax002@ucsd.edu>
2025-05-01 13:20:28 -0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,10 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
-export default function Home() {
-  const [isRed, setIsRed] = useState(true);
-
-  const toggleColor = () => {
-    setIsRed((prev) => !prev);
-  };
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="portfolio">
+        <div className="portfolio">here
           <h2 className="text-2xl font-semibold mb-4">Danny Xu</h2>
           <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <li className="p-4 border rounded shadow hover:shadow-lg transition-shadow">
