026aca21df79affc5222b31ccf05db106c0084d9
Danny Xu <dax002@ucsd.edu>
2025-05-01 12:58:06 -0700

    name

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="portfolio">
-          <h2 className="text-2xl font-semibold mb-4">Portfolio</h2>
+          <h2 className="text-2xl font-semibold mb-4">Danny Xu</h2>
           <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <li className="p-4 border rounded shadow hover:shadow-lg transition-shadow">
               <h3 className="text-lg font-medium">Project 1</h3>
